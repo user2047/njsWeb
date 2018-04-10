034bda89cf17e13d86e306839e853c47dbad1efc
user2047 <cameron.senna@gmail.com>
2018-04-10 15:20:38 -0400

    hbs stuff

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
 const express = require("express");
+const hbs = require("hbs");
+
+var year = new Date().getFullYear();
 
 var app = express();
 
+hbs.registerPartials(__dirname+"/views/partials")
+app.set("view engine","hbs");
 app.use(express.static(__dirname + "/public"));
 
-app.get("/",(req,res)=>{
-  // res.send("<h1>Hello World</h1>");
-  res.send({
-    name: "Cameron",
-    likes: ["running","programming"]
-  });
+hbs.registerHelper("getCurrentYear",()=>{
+  return new Date().getFullYear();
 });
 
+hbs.registerHelper("screamIt",(text)=>{
+  return text.toUpperCase()
+})
+
+var root = {
+  title: "Home",
+  welcomeMessage: "Hello World"
+}
+
+app.get("/",(req,res)=>{
+  res.render("index.hbs",root)
+})
+
+var about = {
+  title:"About",
+  welcomeMessage: "Welcome to the about page"
+}
+
 app.get("/about",(req,res)=>{
-  res.send("im gay");
+  res.render("about.hbs",about);
 });
 
 app.get("/bad",(req,res)=>{
