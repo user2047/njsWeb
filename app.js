cf7941a37acd40951ba9b59c89e5f001ecce57e2
cam sen <cameron.senna@gmail.com>
2018-04-09 23:14:34 -0400

    Second Commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+var app = express();
+
+app.get("/",(req,res)=>{
+  // res.send("<h1>Hello World</h1>");
+  res.send({
+    name: "Cameron",
+    likes: ["running","programming"]
+  });
+});
+
+app.get("/about",(req,res)=>{
+  res.send("im gay");
+});
+
+app.get("/bad",(req,res)=>{
+  res.send({
+    errorMessage:"bad gateway"
+  });
+});
+
+
+
+app.listen(3000,()=>{
+  console.log("serving on port 3000");
+});
