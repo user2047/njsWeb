fd172ea74e42ac886377764c1ed008127662444c
user2047 <cameron.senna@gmail.com>
2018-04-11 20:32:41 -0400

    Added Maintenence file

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const hbs = require("hbs");
+const fs = require("fs");
 
 var year = new Date().getFullYear();
 
@@ -7,7 +8,29 @@ var app = express();
 
 hbs.registerPartials(__dirname+"/views/partials")
 app.set("view engine","hbs");
+
+
+var maintenence = {
+  title: "Under Maintenence"
+}
+// app.use((req,res,next)=>{
+//   res.render("maintenence.hbs",maintenence);
+// });
+
+app.use((req,res,next)=>{
+  var now = new Date().toString();
+  var log = `${now}:${req.method}, ${req.url}`
+  console.log(log);
+  fs.appendFile("server.log", log+"\n",(err)=>{
+    if (err){
+      console.log("Unable to append to server.log");
+    }
+  })
+  next();
+});
+
 app.use(express.static(__dirname + "/public"));
+
 
 hbs.registerHelper("getCurrentYear",()=>{
   return new Date().getFullYear();
